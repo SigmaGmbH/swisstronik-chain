484913e50f5f25b797a7210baa853523e80fa8f5
MikkySnow <mike@swisstronik.com>
2023-02-20 17:32:00 +0100

    add test for counter

--- tests/solidity/test/Counter.js
+++ tests/solidity/test/Counter.js
@@ -12,9 +12,28 @@ describe('Contract tests', () => {
 
         it('Should add', async () => {
             const countBefore = await counterContract.counter()
-            await counterContract.add()
+
+            await expect(counterContract.add())
+                .to.emit(counterContract, 'Added')
+                .to.emit(counterContract, 'Changed')
+
             const countAfter = await counterContract.counter()
             expect(countAfter).to.be.equal(countBefore + 1)
+        })
+
+        it('Should subtract', async () => {
+            const countBefore = await counterContract.counter()
+
+            await expect(counterContract.subtract())
+                .to.emit(counterContract, 'Changed')
+
+            const countAfter = await counterContract.counter()
+            expect(countAfter).to.be.equal(countBefore - 1)
+        })
+
+        it('Should revert correctly', async () => {
+            await expect(counterContract.subtract())
+                .to.be.revertedWith("COUNTER_TOO_LOW")
         })
     })
 })
