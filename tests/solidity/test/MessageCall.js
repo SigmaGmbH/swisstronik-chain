ede6baa9dbe5a57a2e28873861befdbf01ec8390
MikkySnow <mike@swisstronik.com>
2023-02-21 20:09:32 +0100

    comment other tests

--- tests/solidity/test/MessageCall.js
+++ tests/solidity/test/MessageCall.js
@@ -1,18 +1,18 @@
-const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
-const { expect } = require("chai");
-
-describe('Contract tests', () => {
-    describe('Message calls', () => {
-        let contract
-
-        before(async () => {
-            const TestContract = await ethers.getContractFactory('TestMessageCall')
-            contract = await TestContract.deploy()
-        })
-
-        it('Contracts should be able to interact', async () => {
-            const TEST_ITERATIONS = 30
-            await contract.benchmarkMessageCall(TEST_ITERATIONS)
-        })
-    })
-})
+// const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
+// const { expect } = require("chai");
+//
+// describe('Contract tests', () => {
+//     describe('Message calls', () => {
+//         let contract
+//
+//         before(async () => {
+//             const TestContract = await ethers.getContractFactory('TestMessageCall')
+//             contract = await TestContract.deploy()
+//         })
+//
+//         it('Contracts should be able to interact', async () => {
+//             const TEST_ITERATIONS = 30
+//             await contract.benchmarkMessageCall(TEST_ITERATIONS)
+//         })
+//     })
+// })
