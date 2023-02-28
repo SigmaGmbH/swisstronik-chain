945793085f1cc4569ca1c0fd8ca845a95fe413fc
MikkySnow <mike@swisstronik.com>
2023-02-28 16:08:31 +0100

    register EVM account type

--- app/app.go
+++ app/app.go
@@ -347,7 +347,7 @@ func New(
 		appCodec,
 		keys[authtypes.StoreKey],
 		app.GetSubspace(authtypes.ModuleName),
-		authtypes.ProtoBaseAccount,
+		evmcommontypes.ProtoAccount,
 		maccPerms,
 		sdk.Bech32PrefixAccAddr,
 	)
