1f5f6d8129bbea5018b7960b3f595680e605e845
MikkySnow <mike@swisstronik.com>
2023-03-01 17:24:52 +0100

    change contract name to ERC721Token

--- tests/solidity/contracts/ERC721Token.sol
+++ tests/solidity/contracts/ERC721Token.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract GameItem is ERC721URIStorage {
+contract ERC721Token is ERC721URIStorage {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
