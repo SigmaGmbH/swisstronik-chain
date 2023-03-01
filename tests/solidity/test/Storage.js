4b4beeb40023b45a96fd610fbc48b5d79a1a6f92
MikkySnow <mike@swisstronik.com>
2023-03-01 11:35:09 +0100

    uncomment basic solidity tests

--- tests/solidity/test/Storage.js
+++ tests/solidity/test/Storage.js
@@ -1,21 +1,20 @@
-// const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
-// const { expect } = require("chai");
-//
-// describe('Contract tests', () => {
-//     describe('Storage', () => {
-//         let contract
-//
-//         before(async () => {
-//             const TestContract = await ethers.getContractFactory('Storage')
-//             contract = await TestContract.deploy()
-//         })
-//
-//         it('Should be able to set value', async () => {
-//             const value = Math.floor(Math.random() * 10000)
-//             await contract.store(value)
-//
-//             const retrievedValue = await contract.retrieve()
-//             expect(retrievedValue).to.be.equal(value)
-//         })
-//     })
-// })
+const { expect } = require("chai");
+
+describe('Contract tests', () => {
+    describe('Storage', () => {
+        let contract
+
+        before(async () => {
+            const TestContract = await ethers.getContractFactory('Storage')
+            contract = await TestContract.deploy()
+        })
+
+        it('Should be able to set value', async () => {
+            const value = Math.floor(Math.random() * 10000)
+            await contract.store(value)
+
+            const retrievedValue = await contract.retrieve()
+            expect(retrievedValue).to.be.equal(value)
+        })
+    })
+})
