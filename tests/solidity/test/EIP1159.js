51d14dbd893a72e2f26e73012bbdb7f3e6c6a02b
MikkySnow <mike@swisstronik.com>
2023-03-06 16:14:21 +0100

    add test for EIP1159 tx

---
+++ tests/solidity/test/EIP1159.js
@@ -0,0 +1,12 @@
+const {expect} = require('chai')
+
+it('Should send a transaction with EIP-1559 flag', async function () {
+    const [sender, receiver] = await ethers.getSigners()
+    const tx = await sender.sendTransaction({
+        to: receiver.address,
+        value: 100,
+        gasLimit: 21000,
+        type: 2
+    })
+    expect(tx.type).to.be.equal(2)
+});
