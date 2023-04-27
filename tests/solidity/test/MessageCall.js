569db876828def3ea5772dfcd9cd610b38d3c49b
MikkySnow <mike@swisstronik.com>
2023-04-27 12:11:22 +0000

    restore MessageCall test

--- tests/solidity/test/MessageCall.js
+++ tests/solidity/test/MessageCall.js
@@ -1,5 +1,9 @@
+const { sendShieldedTransaction } = require("./testUtils")
+
 describe('Message calls', () => {
     let contract
+    const provider = new ethers.providers.JsonRpcProvider('http://localhost:8535')
+    const signerPrivateKey = '0xC516DC17D909EFBB64A0C4A9EE1720E10D47C1BF3590A257D86EEB5FFC644D43'
 
     before(async () => {
         const TestContract = await ethers.getContractFactory('TestMessageCall')
@@ -9,7 +13,12 @@ describe('Message calls', () => {
 
     it('Contracts should be able to interact', async () => {
         const TEST_ITERATIONS = 30
-        const tx = await contract.benchmarkMessageCall(TEST_ITERATIONS)
+        const tx = await sendShieldedTransaction(
+            provider,
+            signerPrivateKey,
+            contract.address,
+            contract.interface.encodeFunctionData("benchmarkMessageCall", [TEST_ITERATIONS])
+        )
         await tx.wait()
     })
 })
