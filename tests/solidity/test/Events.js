b7a18726e1403675964b6ae58e024c680953c074
MikkySnow <mike@swisstronik.com>
2023-04-27 12:03:08 +0000

    refactor Events test

--- tests/solidity/test/Events.js
+++ tests/solidity/test/Events.js
@@ -1,12 +1,24 @@
-const {expect} = require('chai')
+const { expect } = require('chai')
+const { sendShieldedTransaction } = require("./testUtils")
 
 it('Should emit events correctly', async () => {
+    const provider = new ethers.providers.JsonRpcProvider('http://localhost:8535')
+    const signerPrivateKey = '0xC516DC17D909EFBB64A0C4A9EE1720E10D47C1BF3590A257D86EEB5FFC644D43'
+
     const EventsContract = await ethers.getContractFactory('EventTest')
     const eventInstance = await EventsContract.deploy()
     await eventInstance.deployed()
 
-    await expect(eventInstance.storeWithEvent(888))
-        .to.emit(eventInstance, 'ValueStored1').withArgs(888)
-        .to.emit(eventInstance, 'ValueStored2').withArgs('TestMsg', 888)
-        .to.emit(eventInstance, 'ValueStored3').withArgs('TestMsg', 888, 888)
+    const tx = await sendShieldedTransaction(
+        provider,
+        signerPrivateKey,
+        eventInstance.address,
+        eventInstance.interface.encodeFunctionData("storeWithEvent", [888])
+    )
+    const receipt = await tx.wait()
+    const logs = receipt.logs.map(log => eventInstance.interface.parseLog(log))
+
+    expect(logs.some(log => log.name === 'ValueStored1' && log.args[0].toNumber() == 888)).to.be.true
+    expect(logs.some(log => log.name === 'ValueStored2' && log.args[0] === 'TestMsg' && log.args[1].toNumber() == 888)).to.be.true
+    expect(logs.some(log => log.name === 'ValueStored3' && log.args[0] === 'TestMsg' && log.args[1].toNumber() == 888 && log.args[2].toNumber() == 888)).to.be.true
 })
