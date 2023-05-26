3ca8da41891d67730894473590f6039619c6c987
MikkySnow <mike@swisstronik.com>
2023-05-26 12:48:29 +0000

    fix local node config

--- scripts/local-node.sh
+++ scripts/local-node.sh
@@ -40,6 +40,7 @@ jq '.consensus_params["block"]["max_gas"]="10000000"' "$GENESIS" >"$TMP_GENESIS"
 
 # enable prometheus metrics
 sed -i 's/prometheus = false/prometheus = true/' "$CONFIG"
+sed -i 's/127.0.0.1:26657/0.0.0.0:26657/g' "$CONFIG"
 sed -i 's/prometheus-retention-time  = "0"/prometheus-retention-time  = "1000000000000"/g' "$APP_TOML"
 sed -i 's/enabled = false/enabled = true/g' "$APP_TOML"
 
