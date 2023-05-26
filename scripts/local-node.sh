522192f0637f7d3ad972f4df0c09b2f7796d7f7d
MikkySnow <mike@swisstronik.com>
2023-05-26 12:32:45 +0000

    fix config of local node

--- scripts/local-node.sh
+++ scripts/local-node.sh
@@ -5,7 +5,6 @@ MONIKER="localtestnet"
 KEYRING="test"
 KEYALGO="eth_secp256k1"
 HOMEDIR="$HOME/.swisstronik"
-TRACE=""
 
 # Path variables
 CONFIG=$HOMEDIR/config/config.toml
@@ -37,20 +36,7 @@ jq '.app_state["crisis"]["constant_fee"]["denom"]="uswtr"' "$GENESIS" >"$TMP_GEN
 jq '.app_state["gov"]["deposit_params"]["min_deposit"][0]["denom"]="uswtr"' "$GENESIS" >"$TMP_GENESIS" && mv "$TMP_GENESIS" "$GENESIS"
 jq '.app_state["evm"]["params"]["evm_denom"]="uswtr"' "$GENESIS" >"$TMP_GENESIS" && mv "$TMP_GENESIS" "$GENESIS"
 jq '.app_state["inflation"]["params"]["mint_denom"]="uswtr"' "$GENESIS" >"$TMP_GENESIS" && mv "$TMP_GENESIS" "$GENESIS"
-
-# Set gas limit in genesis
 jq '.consensus_params["block"]["max_gas"]="10000000"' "$GENESIS" >"$TMP_GENESIS" && mv "$TMP_GENESIS" "$GENESIS"
-
-if [[ $1 == "pending" ]]; then
-	sed -i 's/timeout_propose = "3s"/timeout_propose = "30s"/g' "$CONFIG"
-	sed -i 's/timeout_propose_delta = "500ms"/timeout_propose_delta = "5s"/g' "$CONFIG"
-	sed -i 's/timeout_prevote = "1s"/timeout_prevote = "10s"/g' "$CONFIG"
-	sed -i 's/timeout_prevote_delta = "500ms"/timeout_prevote_delta = "5s"/g' "$CONFIG"
-	sed -i 's/timeout_precommit = "1s"/timeout_precommit = "10s"/g' "$CONFIG"
-	sed -i 's/timeout_precommit_delta = "500ms"/timeout_precommit_delta = "5s"/g' "$CONFIG"
-	sed -i 's/timeout_commit = "5s"/timeout_commit = "150s"/g' "$CONFIG"
-	sed -i 's/timeout_broadcast_tx_commit = "10s"/timeout_broadcast_tx_commit = "150s"/g' "$CONFIG"
-fi
 
 # enable prometheus metrics
 sed -i 's/prometheus = false/prometheus = true/' "$CONFIG"
@@ -67,24 +53,15 @@ sed -i.bak 's/pruning = "default"/pruning = "custom"/g' "$APP_TOML"
 sed -i.bak 's/pruning-keep-recent = "0"/pruning-keep-recent = "2"/g' "$APP_TOML"
 sed -i.bak 's/pruning-interval = "0"/pruning-interval = "10"/g' "$APP_TOML"
 
-# Allocate genesis accounts (cosmos formatted addresses)
-for KEY in "${KEYS[@]}"; do
-	swisstronikd add-genesis-account "$KEY" 100000000000000000000000000000000uswtr --keyring-backend $KEYRING --home "$HOMEDIR"
-done
-
-# bc is required to add these big numbers
-total_supply=$(echo "${#KEYS[@]} * 100000000000000000000000000000000uswtr + $amount_to_claim" | bc)
-jq -r --arg total_supply "$total_supply" '.app_state["bank"]["supply"][0]["amount"]=$total_supply' "$GENESIS" >"$TMP_GENESIS" && mv "$TMP_GENESIS" "$GENESIS"
+# Allocate genesis accounts
+swisstronikd add-genesis-account alice 10000000000000000000000000uswtr --keyring-backend $KEYRING --home "$HOMEDIR"
+swisstronikd add-genesis-account bob 10000000000000000000000000uswtr --keyring-backend $KEYRING --home "$HOMEDIR"
 
 # Sign genesis transaction
-swisstronikd gentx "${KEYS[0]}" 100000000000000000000000000000000uswtr --keyring-backend $KEYRING --chain-id $CHAINID --home "$HOMEDIR"
+swisstronikd gentx alice 1000000000000000000000uswtr --keyring-backend $KEYRING --chain-id $CHAINID --home "$HOMEDIR"
 
 # Collect genesis tx
 swisstronikd collect-gentxs --home "$HOMEDIR"
 
 # Run this to ensure everything worked and that the genesis file is setup correctly
 swisstronikd validate-genesis --home "$HOMEDIR"
-
-if [[ $1 == "pending" ]]; then
-	echo "pending mode is on, please wait for the first block committed."
-fi
