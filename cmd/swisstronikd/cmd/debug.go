edd90f5cd62c427fa801dbf06b7b05e8e15f36c5
MikkySnow <mike@swisstronik.com>
2023-09-14 11:07:53 +0000

    refactor: merge debug commands

--- cmd/swisstronikd/cmd/debug.go
+++ cmd/swisstronikd/cmd/debug.go
@@ -1,14 +1,21 @@
 package cmd
 
 import (
-	"fmt"
 	"crypto/ed25519"
 	"crypto/rand"
 	"encoding/base64"
 	"encoding/json"
+	"fmt"
+	"strings"
 
+	"encoding/hex"
 	"github.com/cosmos/cosmos-sdk/client"
+	cryptotypes "github.com/cosmos/cosmos-sdk/crypto/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/version"
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/spf13/cobra"
+	"github.com/tendermint/tendermint/libs/bytes"
 	didutil "swisstronik/testutil/did"
 	didtypes "swisstronik/x/did/types"
 )
@@ -55,6 +62,9 @@ func DebugCmd() *cobra.Command {
 
 	cmd.AddCommand(RandomEd25519PrivateKeypair())
 	cmd.AddCommand(SampleDIDDocument())
+	cmd.AddCommand(ExtractPubkeyCmd())
+	cmd.AddCommand(ConvertAddressCmd())
+
 
 	return cmd
 }
@@ -64,7 +74,7 @@ func RandomEd25519PrivateKeypair() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "random-ed25519-keypair",
 		Short: "Generates random ed25519 keypair",
-		Long: `Generates random ed25519 keypair and outputs it in JSON format with base64-encoded private and public keys. Do not use that keypair in production`,
+		Long:  `Generates random ed25519 keypair and outputs it in JSON format with base64-encoded private and public keys. Do not use that keypair in production`,
 		RunE: func(cmd *cobra.Command, args []string) error {
 			public, private, err := ed25519.GenerateKey(rand.Reader)
 			if err != nil {
@@ -73,10 +83,10 @@ func RandomEd25519PrivateKeypair() *cobra.Command {
 
 			keyPair := struct {
 				PrivateKeyBase64 string `json:"private_key_base_64"`
-				PublicKeyBase64 string `json:"public_key_base_64"`
-			} {
+				PublicKeyBase64  string `json:"public_key_base_64"`
+			}{
 				PrivateKeyBase64: base64.StdEncoding.EncodeToString(private),
-				PublicKeyBase64: base64.StdEncoding.EncodeToString(public),
+				PublicKeyBase64:  base64.StdEncoding.EncodeToString(public),
 			}
 
 			jsonKeyPair, err := json.Marshal(keyPair)
@@ -101,10 +111,10 @@ func SampleDIDDocument() *cobra.Command {
 		RunE: func(cmd *cobra.Command, args []string) error {
 			var (
 				privateKey ed25519.PrivateKey
-				publicKey ed25519.PublicKey
-				err error
+				publicKey  ed25519.PublicKey
+				err        error
 			)
-			// Check if private key was provided. 
+			// Check if private key was provided.
 			if len(args) != 1 {
 				publicKey, privateKey, err = ed25519.GenerateKey(rand.Reader)
 				if err != nil {
@@ -121,7 +131,7 @@ func SampleDIDDocument() *cobra.Command {
 
 			// Generate random DID and key id
 			did := didutil.GenerateDID(didutil.Base58_16bytes)
-			keyId := did + "#key1" 
+			keyId := did + "#key1"
 
 			// Construct verification method
 			verificationMethod := make(map[string]any)
@@ -132,9 +142,9 @@ func SampleDIDDocument() *cobra.Command {
 
 			// Construct DID document
 			document := DIDDocument{
-				Context: []string{"https://www.w3.org/ns/did/v1"},
-				ID: did,
-				Authentication: []string{keyId},
+				Context:            []string{"https://www.w3.org/ns/did/v1"},
+				ID:                 did,
+				Authentication:     []string{keyId},
 				VerificationMethod: []VerificationMethod{verificationMethod},
 			}
 
@@ -144,13 +154,13 @@ func SampleDIDDocument() *cobra.Command {
 			}
 
 			// Construct sign inputs
-			signInputs := SignInput {
+			signInputs := SignInput{
 				VerificationMethodID: keyId,
-				PrivKey: privateKey,
+				PrivKey:              privateKey,
 			}
 
 			result := PayloadWithSignInputs{
-				Payload: encodedDocument,
+				Payload:    encodedDocument,
 				SignInputs: []SignInput{signInputs},
 			}
 
@@ -165,4 +175,69 @@ func SampleDIDDocument() *cobra.Command {
 	}
 
 	return cmd
+}
+
+func ConvertAddressCmd() *cobra.Command {
+	return &cobra.Command{
+		Use:   "convert-address [address]",
+		Short: "Convert an address between hex and bech32",
+		Long:  "Convert an address between hex encoding and bech32.",
+		Args:  cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			addrString := args[0]
+			cfg := sdk.GetConfig()
+
+			var addr []byte
+			switch {
+			case common.IsHexAddress(addrString):
+				addr = common.HexToAddress(addrString).Bytes()
+			case strings.HasPrefix(addrString, cfg.GetBech32ValidatorAddrPrefix()):
+				addr, _ = sdk.ValAddressFromBech32(addrString)
+			case strings.HasPrefix(addrString, cfg.GetBech32AccountAddrPrefix()):
+				addr, _ = sdk.AccAddressFromBech32(addrString)
+			default:
+				return fmt.Errorf("expected a valid hex or bech32 address (acc prefix %s), got '%s'", cfg.GetBech32AccountAddrPrefix(), addrString)
+			}
+
+			cmd.Println("Address bytes:", addr)
+			cmd.Printf("Address (hex): %s\n", bytes.HexBytes(addr))
+			cmd.Printf("Address (EIP-55): %s\n", common.BytesToAddress(addr))
+			cmd.Printf("Bech32 Acc: %s\n", sdk.AccAddress(addr))
+			cmd.Printf("Bech32 Val: %s\n", sdk.ValAddress(addr))
+			return nil
+		},
+	}
+}
+
+// getPubKeyFromString decodes SDK PubKey using JSON marshaler.
+func getPubKeyFromString(ctx client.Context, pkstr string) (cryptotypes.PubKey, error) {
+	var pk cryptotypes.PubKey
+	err := ctx.Codec.UnmarshalInterfaceJSON([]byte(pkstr), &pk)
+	return pk, err
+}
+
+func ExtractPubkeyCmd() *cobra.Command {
+	return &cobra.Command{
+		Use:   "extract-pubkey [pubkey]",
+		Short: "Decode a pubkey from proto JSON",
+		Long:  "Decode a pubkey from proto JSON and display it's address",
+		Example: fmt.Sprintf(
+			`"$ %s debug pubkey '{"@type":"/cosmos.crypto.secp256k1.PubKey","key":"AurroA7jvfPd1AadmmOvWM2rJSwipXfRf8yD6pLbA2DJ"}'`,
+			version.AppName,
+		),
+		Args: cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx := client.GetClientContextFromCmd(cmd)
+			pk, err := getPubKeyFromString(clientCtx, args[0])
+			if err != nil {
+				return err
+			}
+
+			addr := pk.Address()
+			cmd.Printf("Address (EIP-55): %s\n", common.BytesToAddress(addr))
+			cmd.Printf("Bech32 Acc: %s\n", sdk.AccAddress(addr))
+			cmd.Println("PubKey Hex:", hex.EncodeToString(pk.Bytes()))
+			return nil
+		},
+	}
 }
