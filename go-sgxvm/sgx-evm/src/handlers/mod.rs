a61ddc9a09e9c0a3ebea9ba370e03864d4b4f3a1
MikkySnow <mike@swisstronik.com>
2023-09-29 14:17:35 +0000

    refactor: add comments and merge small files into one

--- go-sgxvm/sgx-evm/src/handlers/mod.rs
+++ go-sgxvm/sgx-evm/src/handlers/mod.rs
@@ -1,12 +1,17 @@
-use crate::{AllocationWithResult, Allocation};
 use std::vec::Vec;
 use sgx_types::sgx_status_t;
+
+use crate::{AllocationWithResult, Allocation};
 use crate::ocall;
+use crate::key_manager::KeyManager;
+use crate::protobuf_generated::ffi::NodePublicKeyResponse;
 
 pub mod tx;
-pub mod node;
 
 /// Allocates provided data outside of enclave
+/// * data - bytes to allocate outside of enclave
+/// 
+/// Returns allocation result with pointer to allocated memory, length of allocated data and status of allocation
 pub fn allocate_inner(data: Vec<u8>) -> AllocationWithResult {
     let mut ocall_result = std::mem::MaybeUninit::<Allocation>::uninit();
     let sgx_result = unsafe { 
@@ -30,4 +35,31 @@ pub fn allocate_inner(data: Vec<u8>) -> AllocationWithResult {
             AllocationWithResult::default()
         }
     }
+}
+
+/// Handles incoming request for node public key, which can be used
+/// to derive shared encryption key to encrypt transaction data or 
+/// decrypt node response
+pub fn handle_public_key_request() -> AllocationWithResult {
+    let key_manager = match KeyManager::unseal() {
+        Ok(manager) => manager,
+        Err(err) => {
+            return AllocationWithResult::default()
+        }
+    };
+
+    let public_key = key_manager.get_public_key();
+
+    let mut response = NodePublicKeyResponse::new();
+    response.set_publicKey(public_key);
+
+    let encoded_response = match response.write_to_bytes() {
+        Ok(res) => res,
+        Err(err) => {
+            println!("Cannot encode protobuf result. Reason: {:?}", err);
+            return AllocationWithResult::default();
+        }
+    };
+    
+    super::allocate_inner(encoded_response)
 }
