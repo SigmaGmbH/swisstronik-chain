9028dab0eeaf30479459083fcdbcab839e47aecd
MikkySnow <mike@swisstronik.com>
2023-10-30 11:53:18 +0000

    test: update VC.sol test contract

--- tests/solidity/contracts/VC.sol
+++ tests/solidity/contracts/VC.sol
@@ -1,20 +1,25 @@
 pragma solidity ^0.8;
 
+/**
+ Sample contract to test if precompile for verifiable credentials
+*/
 contract VC {
     mapping (address => bool) private _isAuthorized;
 
-    function authorize(bytes calldata credential) public {
-        (bool passed, bytes memory data) = address(1027).staticcall(credential);
-        require(passed, "Cannot verify credential");
+    /**
+        Verifies provided JWT Proof for Verifiable Credential.
+        Returns `address` of credential subject 
+     */
+    function verifyJWT(bytes calldata credential) public view returns (address) {
+        (bool success, bytes memory data) = address(1027).staticcall(credential);
+        require(success, "Cannot verify credential");
 
-        address credentialSubject = bytesToAddress(data);
-        _isAuthorized[credentialSubject] = true;
+        return bytesToAddress(data);
     }
 
-    function isAuthorized(address user) public view returns (bool) {
-        return _isAuthorized[user];
-    }
-
+    /**
+        Converts provided bytes array to address
+     */
     function bytesToAddress(bytes memory b) private pure returns (address) {
         return address(uint160(bytes20(b)));
     }  
