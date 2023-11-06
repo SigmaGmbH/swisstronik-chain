d5b4f793587afe220c4408cc7de388c1c92e85c6
kenta92115 <kenta@swisstronik.com>
2023-11-06 04:19:13 +0000

    chore: set eth key type to legacy Cdc

--- crypto/codec/amino.go
+++ crypto/codec/amino.go
@@ -17,6 +17,7 @@ package codec
 
 import (
 	"github.com/cosmos/cosmos-sdk/codec"
+	"github.com/cosmos/cosmos-sdk/codec/legacy"
 	cryptocodec "github.com/cosmos/cosmos-sdk/crypto/codec"
 	"github.com/cosmos/cosmos-sdk/crypto/keyring"
 
@@ -33,4 +34,8 @@ func RegisterCrypto(cdc *codec.LegacyAmino) {
 
 	keyring.RegisterLegacyAminoCodec(cdc)
 	cryptocodec.RegisterCrypto(cdc)
+
+	// NOTE: update SDK's amino codec to include the ethsecp256k1 keys.
+	// DO NOT REMOVE unless deprecated on the SDK.
+	legacy.Cdc = cdc
 }
