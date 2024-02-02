409c5ee67f56b40103ff4149ab1f0a579d0f35d5
MikkySnow <mike@swisstronik.com>
2024-02-02 13:02:50 +0000

    feat: add draft of status command

--- cmd/swisstronikd/cmd/enclave.go
+++ cmd/swisstronikd/cmd/enclave.go
@@ -1,14 +1,14 @@
 package cmd
 
 import (
-	"net"
 	"fmt"
+	"net"
 	"strconv"
 
+	"github.com/SigmaGmbH/librustgo"
 	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/cosmos/cosmos-sdk/server"
 	"github.com/spf13/cobra"
-	"github.com/SigmaGmbH/librustgo"
 )
 
 const flagShouldReset = "reset"
@@ -24,6 +24,7 @@ func EnclaveCmd() *cobra.Command {
 	cmd.AddCommand(RequestMasterKeyCmd())
 	cmd.AddCommand(CreateMasterKey())
 	cmd.AddCommand(StartAttestationServer())
+	cmd.AddCommand(Status())
 
 	return cmd
 }
@@ -33,8 +34,8 @@ func RequestMasterKeyCmd() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "request-master-key [bootstrap-node-address]",
 		Short: "Requests master key from bootstrap node",
-		Long: `Initializes SGX enclave by passing process of Remote Attestation agains bootstrap node. If remote attestation was successful, bootstrap node shares encrypted master key with this node. Process of Remote Attestation is performed over pure TCP protocol.`,
-		Args: cobra.ExactArgs(1),
+		Long:  "Initializes SGX enclave by passing process of Remote Attestation agains bootstrap node. If remote attestation was successful, bootstrap node shares encrypted master key with this node. Process of Remote Attestation is performed over pure TCP protocol.",
+		Args:  cobra.ExactArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			clientCtx := client.GetClientContextFromCmd(cmd)
 			serverCtx := server.GetServerContextFromCmd(cmd)
@@ -68,9 +69,9 @@ func CreateMasterKey() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "create-master-key",
 		Short: "Creates new master key",
-		Long: `Initializes SGX enclave by creating new master key. Use this function for first validator in network`,
-		Args: cobra.ExactArgs(0),
-		RunE: func(cmd *cobra.Command, args []string) error {
+		Long:  "Initializes SGX enclave by creating new master key. Use this function for first validator in network",
+		Args:  cobra.ExactArgs(0),
+		RunE: func(cmd *cobra.Command, _ []string) error {
 			shouldReset, err := cmd.Flags().GetBool(flagShouldReset)
 			if err != nil {
 				return err
@@ -94,15 +95,29 @@ func CreateMasterKey() *cobra.Command {
 // StartAttestationServer returns start-attestation-server cobra Command.
 func StartAttestationServer() *cobra.Command {
 	cmd := &cobra.Command{
-		Use: "start-attestation-server [address-with-port]",
+		Use:   "start-attestation-server [address-with-port]",
 		Short: "Starts attestation server",
-		Long: "Start server for Intel SGX Remote Attestation to share master key with new nodes",
-		Args: cobra.ExactArgs(1),
-		RunE: func(cmd *cobra.Command, args []string) error {
+		Long:  "Start server for Intel SGX Remote Attestation to share master key with new nodes",
+		Args:  cobra.ExactArgs(1),
+		RunE: func(_ *cobra.Command, args []string) error {
 			if err := librustgo.StartSeedServer(args[0]); err != nil {
 				return err
 			}
 			return server.WaitForQuitSignals()
+		},
+	}
+
+	return cmd
+}
+
+// Healthcheck checks if Intel SGX Enclave is accessible and if Intel SGX was properly configured
+func Status() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "status",
+		Short: "Checks status of Intel SGX Enclave",
+		Long:  "Checks if Intel SGX Enclave is accessible and if Intel SGX was properly configured",
+		RunE: func(cmd *cobra.Command, _ []string) error {
+			return nil
 		},
 	}
 
