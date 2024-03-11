7403cf675c5fa6709824292f805a25acd6ae4599
MikkySnow <mike@swisstronik.com>
2024-03-11 14:24:53 +0000

    feat: add basic getter and setter

--- x/compliance/keeper/keeper.go
+++ x/compliance/keeper/keeper.go
@@ -2,12 +2,15 @@ package keeper
 
 import (
 	"fmt"
+	"github.com/cosmos/gogoproto/proto"
 
 	"github.com/cometbft/cometbft/libs/log"
+	"github.com/cosmos/cosmos-sdk/store/prefix"
 	storetypes "github.com/cosmos/cosmos-sdk/store/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	paramtypes "github.com/cosmos/cosmos-sdk/x/params/types"
 
+	"github.com/ethereum/go-ethereum/common"
 	"swisstronik/x/compliance/types"
 )
 
@@ -29,12 +32,57 @@ func NewKeeper(
 	}
 
 	return &Keeper{
-		storeKey:      storeKey,
-		memKey:        memKey,
-		paramstore:    ps,
+		storeKey:   storeKey,
+		memKey:     memKey,
+		paramstore: ps,
 	}
 }
 
 func (k Keeper) Logger(ctx sdk.Context) log.Logger {
 	return ctx.Logger().With("module", fmt.Sprintf("x/%s", types.ModuleName))
+}
+
+func (k *Keeper) AddVerificationEntry(ctx sdk.Context, subjectAddress, issuerAddress common.Address, originChain string) error {
+	store := prefix.NewStore(ctx.KVStore(k.storeKey), types.KeyPrefixVerification)
+
+	adapterData := types.IssuerAdapterContractDetail{
+		IssuerAlias:     issuerAddress.String(),
+		ContractAddress: issuerAddress.Bytes(),
+	}
+
+	verificationEntry := &types.VerificationEntry{
+		AdapterData:         &adapterData,
+		OriginChain:         originChain,
+		IssuanceTimestamp:   uint32(ctx.BlockHeader().Time.Unix()),
+		ExpirationTimestamp: 0,
+		OriginalData:        nil,
+	}
+
+	verificationData := types.VerificationData{
+		VerificationType: types.VerificationType_VT_KYC,
+		Entries:          []*types.VerificationEntry{verificationEntry},
+	}
+	verificationDataBytes, err := verificationData.Marshal()
+	if err != nil {
+		return err
+	}
+
+	store.Set(subjectAddress.Bytes(), verificationDataBytes)
+	return nil
+}
+
+func (k *Keeper) GetVerificationData(ctx sdk.Context, subjectAddress common.Address) (*types.VerificationData, error) {
+	store := prefix.NewStore(ctx.KVStore(k.storeKey), types.KeyPrefixVerification)
+
+	verificationDataBytes := store.Get(subjectAddress.Bytes())
+	if verificationDataBytes == nil {
+		return &types.VerificationData{}, nil
+	}
+
+	var verificationData types.VerificationData
+	if err := proto.Unmarshal(verificationDataBytes, &verificationData); err != nil {
+		return nil, err
+	}
+
+	return &verificationData, nil
 }
