dc1ca39842bec216e3c985322ab56f6397ce1493
MikkySnow <mike@swisstronik.com>
2024-03-15 10:44:47 +0000

    feat: add route function for compliance precompile

--- sgxvm/src/precompiles/compliance_bridge.rs
+++ sgxvm/src/precompiles/compliance_bridge.rs
@@ -1,13 +1,23 @@
 extern crate sgx_tstd as std;
 
+use alloc::borrow::ToOwned;
+use alloc::string::ToString;
 use evm::executor::stack::{PrecompileHandle, PrecompileOutput};
+use evm::{ExitError, ExitRevert};
+use primitive_types::H160;
 use std::prelude::v1::*;
 use std::vec::Vec;
+use ethabi::{Function, Param, ParamType, Token as AbiToken};
 
 use crate::GoQuerier;
 use crate::precompiles::{
     ExitSucceed, LinearCostPrecompileWithQuerier, PrecompileFailure, PrecompileResult,
 };
+
+// Selector of addVerificationDetails function
+const ADD_VERIFICATION_FN_SELECTOR: &str = "455d0d34";
+// Selector of hasVerification function
+const HAS_VERIFICATION_FN_SELECTOR: &str = "4887fcd8";
 
 /// Precompile for interactions with x/compliance module.
 pub struct ComplianceBridge;
@@ -26,7 +36,12 @@ impl LinearCostPrecompileWithQuerier for ComplianceBridge {
         )?;
 
         handle.record_cost(cost)?;
-        let (exit_status, output) = execute_inner(querier, handle.input())?;
+
+
+
+
+        let context = handle.context();
+        let (exit_status, output) = route(querier, context.address, handle.input())?;
         Ok(PrecompileOutput {
             exit_status,
             output,
@@ -34,9 +49,29 @@ impl LinearCostPrecompileWithQuerier for ComplianceBridge {
     }
 }
 
-fn execute_inner(
-    querier: *mut GoQuerier,
-    input: &[u8],
-) -> Result<(ExitSucceed, Vec<u8>), PrecompileFailure> {
-    Ok((ExitSucceed::Returned, Vec::default()))
+fn route(querier: *mut GoQuerier, caller: H160, data: &[u8]) -> Result<(ExitSucceed, Vec<u8>), PrecompileFailure> {
+    if data.len() <= 4 {
+        return Err(PrecompileFailure::Error {
+            exit_status: ExitError::Other("cannot decode input".into()),
+        })
+    }
+
+    let input_signature = hex::encode(data[..4].to_vec());
+    match input_signature.as_str() {
+        HAS_VERIFICATION_FN_SELECTOR => {
+            // TODO: Decode params
+            // TODO: Implement READ from x/compliance
+            Ok((ExitSucceed::Returned, Vec::default()))
+        },
+        ADD_VERIFICATION_FN_SELECTOR => {
+            // TODO: Decode params
+            // TODO: Implement WRITE to x/compliance
+            Ok((ExitSucceed::Returned, Vec::default()))
+        },
+        _ => {
+            Err(PrecompileFailure::Error {
+                exit_status: ExitError::Other("cannot decode input".into()),
+            })
+        }
+    }
 }
