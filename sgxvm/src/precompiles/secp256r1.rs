6eb7df6c09fd04d27c206427c6233a84f655ffe3
Denis <dev-adv@swisstronik.com>
2024-03-18 12:24:03 +0400

    Fix secp256r1 impl

--- sgxvm/src/precompiles/secp256r1.rs
+++ sgxvm/src/precompiles/secp256r1.rs
@@ -1,3 +1,5 @@
+extern crate sgx_tstd as std;
+
 use std::vec::Vec;
 use core::cmp::min;
 use p256::ecdsa::{signature::hazmat::PrehashVerifier, Signature, VerifyingKey};
@@ -79,7 +81,7 @@ mod tests {
                 assert_eq!(
                     e,
                     PrecompileFailure::Error {
-                        exit_status: ExitError::Other("input must contain 128 bytes".into())
+                        exit_status: ExitError::Other("input must contain 160 bytes".into())
                     }
                 );
                 Ok(())
@@ -90,11 +92,13 @@ mod tests {
     fn proper_sig_verify() {
         let input = hex::decode("4cee90eb86eaa050036147a12d49004b6b9c72bd725d39d4785011fe190f0b4da73bd4903f0ce3b639bbbf6e8e80d16931ff4bcf5993d58468e8fb19086e8cac36dbcd03009df8c59286b162af3bd7fcc0450c9aa81be5d10d312af6c66b1d604aebd3099c618202fcfe16ae7770b0c49ab5eadf74b754204a3bb6060e44eff37618b065f9832de4ca6ca971a7a1adc826d0f7c00181a5fb2ddf79ae00b4e10e").unwrap();
         let target_gas = 3_500u64;
-        let (gas_used, res) = P256Verify::raw_execute(&input, target_gas).unwrap();
-        assert_eq!(gas_used, 3_450u64);
-        let mut expected_res = [0u8; 32];
-        expected_res[31] = 1;
-        assert_eq!(res, expected_res.to_vec());
+        let (success, res) = P256Verify::raw_execute(&input, target_gas).unwrap();
+        assert_eq!(success, ExitSucceed::Returned);
+        assert_eq!(res.len(), 4);
+        assert_eq!(res[0], 0u8);
+        assert_eq!(res[1], 0u8);
+        assert_eq!(res[2], 0u8);
+        assert_eq!(res[3], 0u8);
     }
 
 }
