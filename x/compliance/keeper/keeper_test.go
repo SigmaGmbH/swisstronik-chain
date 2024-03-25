b862f312c0a8a038dd24d2354fec473865bb4c23
Denis <dev-adv@swisstronik.com>
2024-03-25 19:06:30 +0400

    Add some tests for compliance keeper, compliance params test

--- x/compliance/keeper/keeper_test.go
+++ x/compliance/keeper/keeper_test.go
@@ -47,9 +47,96 @@ func (suite *KeeperTestSuite) Setup(t *testing.T) {
 }
 
 func (suite *KeeperTestSuite) TestCreateSimpleAndFetchSimpleIssuer() {
-
 	details := &types.IssuerDetails{Name: "testIssuer"}
 	from, _ := tests.RandomEthAddressWithPrivateKey()
-	err := suite.keeper.SetIssuerDetails(suite.ctx,sdk.AccAddress(from.Bytes()), details)
+	address := sdk.AccAddress(from.Bytes())
+	err := suite.keeper.SetIssuerDetails(suite.ctx, address, details)
+	suite.Require().NoError(err)
+	i, err := suite.keeper.GetIssuerDetails(suite.ctx, address)
+	suite.Require().Equal(details, i)
+	suite.Require().NoError(err)
+	suite.keeper.RemoveIssuer(suite.ctx, address)
+	i, err = suite.keeper.GetIssuerDetails(suite.ctx, address)
+	suite.Require().Equal("", i.Name)
+	suite.Require().NoError(err)
+}
+
+func (suite *KeeperTestSuite) TestNonExistingIssuer() {
+	from, _ := tests.RandomEthAddressWithPrivateKey()
+	address := sdk.AccAddress(from.Bytes())
+	i, err := suite.keeper.GetIssuerDetails(suite.ctx, address)
+	suite.Require().Equal("", i.Name)
+	suite.Require().NoError(err)
+}
+
+func (suite *KeeperTestSuite) TestAddressDetailsCRUD() {
+	from, _ := tests.RandomEthAddressWithPrivateKey()
+	address := sdk.AccAddress(from.Bytes())
+	details := &types.AddressDetails{IsVerified: true,
+		IsRevoked: false,
+		Verifications: []*types.Verification{{
+			Type:           types.VerificationType_VT_KYC,
+			VerificationId: nil,
+			IssuerAddress:  from.String(),
+		}}}
+	err := suite.keeper.SetAddressDetails(suite.ctx, address, details)
+	suite.Require().NoError(err)
+	i, err := suite.keeper.GetAddressDetails(suite.ctx, address)
+	suite.Require().Equal(details, i)
+	suite.Require().NoError(err)
+}
+
+func (suite *KeeperTestSuite) TestAddressVerified() {
+	from, _ := tests.RandomEthAddressWithPrivateKey()
+	address := sdk.AccAddress(from.Bytes())
+	details := &types.AddressDetails{IsVerified: true,
+		IsRevoked: false,
+		Verifications: make([]*types.Verification, 0)}
+	err := suite.keeper.SetAddressDetails(suite.ctx, address, details)
+	suite.Require().NoError(err)
+	i, err := suite.keeper.IsAddressVerified(suite.ctx, address)
+	suite.Require().Equal(true, i)
+	from2, _ := tests.RandomEthAddressWithPrivateKey()
+	address2 := sdk.AccAddress(from2.Bytes())
+	details2 := &types.AddressDetails{IsVerified: false,
+		IsRevoked: false,
+		Verifications: make([]*types.Verification, 0)}
+	err = suite.keeper.SetAddressDetails(suite.ctx, address2, details2)
+	suite.Require().NoError(err)
+	i, err = suite.keeper.IsAddressVerified(suite.ctx, address2)
+	suite.Require().Equal(false, i)
+}
+
+
+func (suite *KeeperTestSuite) TestAddressDetailsSetVerificationStatus() {
+	from, _ := tests.RandomEthAddressWithPrivateKey()
+	address := sdk.AccAddress(from.Bytes())
+	details := &types.AddressDetails{
+		IsVerified: false,
+		IsRevoked: false,
+		Verifications: []*types.Verification{{
+			Type:           types.VerificationType_VT_KYC,
+			VerificationId: nil,
+			IssuerAddress:  from.String(),
+		}}}
+	err := suite.keeper.SetAddressDetails(suite.ctx, address, details)
+	suite.Require().NoError(err)
+	// set to true
+	err = suite.keeper.SetAddressVerificationStatus(suite.ctx,address, true)
+	suite.Require().NoError(err)
+	i, err := suite.keeper.GetAddressDetails(suite.ctx, address)
+	suite.Require().Equal(true, i.IsVerified)
+	suite.Require().NoError(err)
+	// set to false
+	err = suite.keeper.SetAddressVerificationStatus(suite.ctx,address, false)
+	suite.Require().NoError(err)
+	i, err = suite.keeper.GetAddressDetails(suite.ctx, address)
+	suite.Require().Equal(false, i.IsVerified)
+	suite.Require().NoError(err)
+	// NOOP
+	err = suite.keeper.SetAddressVerificationStatus(suite.ctx,address, false)
+	suite.Require().NoError(err)
+	i, err = suite.keeper.GetAddressDetails(suite.ctx, address)
+	suite.Require().Equal(false, i.IsVerified)
 	suite.Require().NoError(err)
 }
