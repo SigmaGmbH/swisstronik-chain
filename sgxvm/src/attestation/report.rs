a0d24a3a438c17f13400cf304025fb5238ebe964
MikkySnow <mike@swisstronik.com>
2024-04-12 12:32:30 +0000

    refactor: rollback to v4

--- sgxvm/src/attestation/report.rs
+++ sgxvm/src/attestation/report.rs
@@ -132,8 +132,8 @@ pub struct AttestationReport {
     pub sgx_quote_body: SgxQuote,
     pub platform_info_blob: Option<Vec<u8>>,
     pub advisory_ids: AdvisoryIDs,
-    /// TCB Evaluation Data number
-    pub tcb: u64,
+    // TCB Evaluation Data number
+    // pub tcb: u64,
 }
 
 impl AttestationReport {
@@ -255,11 +255,11 @@ impl AttestationReport {
             vec![]
         };
 
-        // Extract TCB
-        let tcb = attn_report["tcbEvaluationDataNumber"].as_u64().ok_or_else(|| {
-            println!("Error extracting TCB");
-            Error::ReportParseError
-        })?;
+        // TODO: Extract TCB
+        // let tcb = attn_report["tcbEvaluationDataNumber"].as_u64().ok_or_else(|| {
+        //     println!("Error extracting TCB");
+        //     Error::ReportParseError
+        // })?;
 
         // Extract timestamp from report
         let timestamp = attn_report["timestamp"].as_str().ok_or_else(|| {
@@ -286,7 +286,7 @@ impl AttestationReport {
             sgx_quote_body,
             platform_info_blob,
             advisory_ids: AdvisoryIDs(advisories),
-            tcb,
+            // tcb,
         })
     }
 }
