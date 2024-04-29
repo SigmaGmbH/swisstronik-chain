c02728db10fd0c98c7678824d1ca58c4df6008dc
MikkySnow <mike@swisstronik.com>
2024-04-29 12:51:31 +0000

    feat: add draft CLI commands

--- go-sgxvm/cmd/attestation/cmd/root.go
+++ go-sgxvm/cmd/attestation/cmd/root.go
@@ -13,6 +13,9 @@ func RootCmd() *cobra.Command {
 
 	cmd.AddCommand(
 		StartAttestationServer(),
+		AddNewEpoch(),
+		ListEpochs(),
+		RemoveLatestEpoch(),
 	)
 
 	return cmd
@@ -30,6 +33,49 @@ func StartAttestationServer() *cobra.Command {
 				return err
 			}
 			return WaitForQuitSignals()
+		},
+	}
+
+	return cmd
+}
+
+// AddNewEpoch returns create-epoch-key cobra Command.
+func AddNewEpoch() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "add-new-epoch [starting-block]",
+		Short: "Creates new epoch",
+		Long:  "Creates new epoch inside Intel SGX Enclave",
+		Args:  cobra.ExactArgs(1),
+		Run: func(_ *cobra.Command, args []string) {
+			// TODO: Implement
+		},
+	}
+
+	return cmd
+}
+
+// ListEpochs returns list-epochs cobra Command.
+func ListEpochs() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "list-epochs",
+		Short: "Lists all stored epochs",
+		Long:  "Lists all stored epochs with their starting blocks",
+		Run: func(_ *cobra.Command, args []string) {
+			// TODO: Implement
+		},
+	}
+
+	return cmd
+}
+
+// RemoveLatestEpoch returns remove-epoch cobra Command.
+func RemoveLatestEpoch() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "remove-latest-epoch",
+		Short: "Removes latest epoch ",
+		Long:  "Allows to remove latest epoch, for example in case, if epoch starting block was set incorrectly",
+		Run: func(_ *cobra.Command, args []string) {
+			// TODO: Implement
 		},
 	}
 
--- go.mod
+++ go.mod
@@ -1,6 +1,8 @@
 module swisstronik
 
-go 1.20
+go 1.21
+
+toolchain go1.21.3
 
 require (
 	cosmossdk.io/api v0.3.1
@@ -45,7 +47,7 @@ require (
 	github.com/regen-network/cosmos-proto v0.3.1
 	github.com/rs/cors v1.9.0
 	github.com/spf13/cast v1.5.1
-	github.com/spf13/cobra v1.7.0
+	github.com/spf13/cobra v1.8.0
 	github.com/spf13/pflag v1.0.5
 	github.com/spf13/viper v1.16.0
 	github.com/status-im/keycard-go v0.2.0
