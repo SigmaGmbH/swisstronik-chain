6ebcaa4a2e2e4ee5e8b5b92865829de059ac85d7
Hungry Warrior <hungrywarrior081@gmail.com>
2024-06-24 12:35:52 -0400

    Setup chain upgrade handler

---
+++ app/upgrades/v1_0_3/constants.go
@@ -0,0 +1,6 @@
+package v1_0_3
+
+const (
+	UpgradeName = "v1.0.3"
+	UpgradeInfo = "{\\\"binaries\\\":{\\\"linux/amd64\\\":\\\"http://swisstronik-server/swisstronikd_v1.0.3.tar.gz?checksum=sha256:05c3372cbe5fb54b53de5075b7f92ca174e44cc95ab0dec0e1481cbd4339b46a\\\"}}"
+)
---
+++ app/upgrades/v1_0_3/upgrades.go
@@ -0,0 +1,17 @@
+package v1_0_3
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/module"
+	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
+)
+
+func CreateUpgradeHandler(
+	mm *module.Manager,
+	configurator module.Configurator,
+) upgradetypes.UpgradeHandler {
+	return func(ctx sdk.Context, _ upgradetypes.Plan, vm module.VersionMap) (module.VersionMap, error) {
+		// Leave modules as-is to avoid running InitGenesis.
+		return mm.RunMigrations(ctx, configurator, vm)
+	}
+}
