918d8dcaf40b9c7027d7ab78322accd731d02d58
Hungry Warrior <hungrywarrior081@gmail.com>
2024-08-19 09:07:23 +0000

    Call markUserAsVerified() by eth_call

--- tests/solidity/test/ComplianceBridge.js
+++ tests/solidity/test/ComplianceBridge.js
@@ -1,4 +1,4 @@
-const { expect } = require("chai");
+const { expect } = require("chai")
 const { ethers } = require("hardhat")
 const { sendShieldedTransaction, sendShieldedQuery } = require("./testUtils")
 
@@ -8,29 +8,70 @@ describe('ComplianceBridge', () => {
     before(async () => {
         const [signer] = await ethers.getSigners()
         const ComplianceProxyFactory = await ethers.getContractFactory('ComplianceProxy')
-        contract = new ethers.Contract("0x30252afe8c1683fd184c99a3c44aa5d547d59dd4", ComplianceProxyFactory.interface, signer);
+        contract = new ethers.Contract("0x30252afe8c1683fd184c99a3c44aa5d547d59dd4", ComplianceProxyFactory.interface, signer)
     })
 
     it('Should be able to add verification details', async () => {
         const [signer] = await ethers.getSigners()
+
+        // Make sure that signer was not verified yet
+        const isVerifiedRespBeforeCall = await sendShieldedQuery(
+            signer.provider,
+            contract.address,
+            contract.interface.encodeFunctionData("isUserVerified", [signer.address])
+        )
+        const isVerifiedBeforeCall = contract.interface.decodeFunctionResult("isUserVerified", isVerifiedRespBeforeCall)
+        expect(isVerifiedBeforeCall[0]).to.be.false
+
+        // Try to call `markUserAsVerified` by using `eth_call`
+        const markUserAsVerifiedResp = await sendShieldedQuery(
+            signer.provider,
+            contract.address,
+            contract.interface.encodeFunctionData("markUserAsVerified", [signer.address])
+        )
+        const verificationId = contract.interface.decodeFunctionResult("markUserAsVerified", markUserAsVerifiedResp)
+        expect(verificationId.length).to.be.gt(0)
+
+        // Confirm that verified status was not changed yet
+        const isVerifiedRespAfterCall = await sendShieldedQuery(
+            signer.provider,
+            contract.address,
+            contract.interface.encodeFunctionData("isUserVerified", [signer.address])
+        )
+        const isVerifiedAfterCall = contract.interface.decodeFunctionResult("isUserVerified", isVerifiedRespAfterCall)
+        expect(isVerifiedAfterCall[0]).to.be.false
+
+        // const gas = await contract.estimateGas.markUserAsVerified(signer.address)
+        // console.log('eth_estimateGas(markUserAsVeried)', gas)
+        //
+        // // Confirm that verified status was not changed yet
+        // const isVerifiedRespAfterEstimateGas = await sendShieldedQuery(
+        //     signer.provider,
+        //     contract.address,
+        //     contract.interface.encodeFunctionData("isUserVerified", [signer.address])
+        // )
+        // const isVerifiedAfterEstimateGas = contract.interface.decodeFunctionResult("isUserVerified", isVerifiedRespAfterEstimateGas)
+        // expect(isVerifiedAfterEstimateGas[0]).to.be.false
 
         const tx = await sendShieldedTransaction(
             signer,
             contract.address,
             contract.interface.encodeFunctionData("markUserAsVerified", [signer.address])
         )
+        // What happens after tx confirmation
         const res = await tx.wait()
         const parsedLog = contract.interface.parseLog(res.logs[0])
         expect(parsedLog.args.success).to.be.true
-        expect(parsedLog.args.data.length).to.be.greaterThan(0);
+        expect(parsedLog.args.data.length).to.be.greaterThan(0)
 
-        const isVerifiedResponse = await sendShieldedQuery(
+        // Confirm that verified status was changed after tx confirmation
+        const isVerifiedRespAfterTx = await sendShieldedQuery(
             signer.provider,
             contract.address,
             contract.interface.encodeFunctionData("isUserVerified", [signer.address])
         )
-        const result = contract.interface.decodeFunctionResult("isUserVerified", isVerifiedResponse)
-        expect(result[0]).to.be.true
+        const isVerifiedAfterTx = contract.interface.decodeFunctionResult("isUserVerified", isVerifiedRespAfterTx)
+        expect(isVerifiedAfterTx[0]).to.be.true
     })
 
     it('Should be able to check for specific issuer of verification', async () => {
@@ -67,7 +108,7 @@ describe('ComplianceBridge', () => {
             contract.interface.encodeFunctionData("getVerificationData", [signer.address])
         )
         const result = contract.interface.decodeFunctionResult("getVerificationData", verificationData)
-        expect(result[0].length).to.be.greaterThan(0);
+        expect(result[0].length).to.be.greaterThan(0)
         for (const details of result[0]) {
             expect(details.issuerAddress.length).to.be.greaterThan(0)
             expect(details.issuerAddress.toLowerCase()).to.be.equal(contract.address.toLowerCase())
