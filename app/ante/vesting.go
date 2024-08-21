5c41c5a17ce61c7984c984eb74e1fee679f2d4d9
Hungry Warrior <hungrywarrior081@gmail.com>
2024-08-21 09:05:40 +0000

    Refactor vesting ante handler

--- app/ante/vesting.go
+++ app/ante/vesting.go
@@ -4,11 +4,8 @@ import (
 	"math/big"
 
 	errorsmod "cosmossdk.io/errors"
-	"cosmossdk.io/math"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	errortypes "github.com/cosmos/cosmos-sdk/types/errors"
-	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
-
 	evmtypes "swisstronik/x/evm/types"
 	vestingmoduletypes "swisstronik/x/vesting/types"
 )
@@ -19,15 +16,6 @@ type EthVestingTransactionDecorator struct {
 	ak evmtypes.AccountKeeper
 	bk evmtypes.BankKeeper
 	ek EVMKeeper
-}
-
-// EthVestingExpenseTracker tracks both the total transaction value to be sent across Ethereum
-// messages and the maximum spendable value for a given account.
-type EthVestingExpenseTracker struct {
-	// Total is the total value to be spent across a transaction with one or more Ethereum message calls
-	Total *big.Int
-	// Spendable is the maximum value that can be spent
-	Spendable *big.Int
 }
 
 // NewEthVestingTransactionDecorator returns a new EthVestingTransactionDecorator.
@@ -42,108 +30,54 @@ func NewEthVestingTransactionDecorator(ak evmtypes.AccountKeeper, bk evmtypes.Ba
 // AnteHandle validates that monthly vesting account has sufficient unlocked balances to cover the transaction
 // during the cliff and vesting period.
 func (vtd EthVestingTransactionDecorator) AnteHandle(ctx sdk.Context, tx sdk.Tx, simulate bool, next sdk.AnteHandler) (sdk.Context, error) {
-	accountExpenses := make(map[string]*EthVestingExpenseTracker)
+	accountSpendable := make(map[string]*big.Int)
 	denom := vtd.ek.GetParams(ctx).EvmDenom
 
+	// Aggregate all the spending amounts per each account.
 	for _, msg := range tx.GetMsgs() {
 		msgEthTx, ok := msg.(*evmtypes.MsgHandleTx)
 		if !ok {
 			return ctx, errorsmod.Wrapf(errortypes.ErrUnknownRequest, "invalid message type %T, expected %T", msg, (*evmtypes.MsgHandleTx)(nil))
 		}
 
-		txData, err := evmtypes.UnpackTxData(msgEthTx.Data)
+		_, err := evmtypes.UnpackTxData(msgEthTx.Data)
 		if err != nil {
 			return ctx, errorsmod.Wrap(err, "failed to unpack tx data")
 		}
 
 		from := msgEthTx.GetFrom()
-		value := txData.GetValue()
-		acc := vtd.ak.GetAccount(ctx, from)
-		if acc == nil {
-			return ctx, errorsmod.Wrapf(errortypes.ErrUnknownAddress, "account %s does not exist", acc)
+		account := vtd.ak.GetAccount(ctx, from)
+		// Only check if account is vesting account
+		if _, ok = account.(*vestingmoduletypes.MonthlyVestingAccount); !ok {
+			continue
 		}
+		fromStr := from.String()
 
-		if err = CheckVesting(ctx, vtd.bk, acc, accountExpenses, value, denom); err != nil {
-			return ctx, err
+		spendable := vtd.bk.SpendableCoin(ctx, from, denom)
+		if existing, ok := accountSpendable[fromStr]; ok {
+			accountSpendable[fromStr] = new(big.Int).Add(existing, spendable.Amount.BigInt())
+		} else {
+			accountSpendable[fromStr] = spendable.Amount.BigInt()
 		}
+	}
+
+	// Check if all the vesting accounts have enough spendable amount as what they need.
+	for _, msg := range tx.GetMsgs() {
+		msgEthTx, _ := msg.(*evmtypes.MsgHandleTx)
+		txData, _ := evmtypes.UnpackTxData(msgEthTx.Data)
+
+		from := msgEthTx.GetFrom()
+		account := vtd.ak.GetAccount(ctx, from)
+		// Only check if account is vesting account
+		if _, ok := account.(*vestingmoduletypes.MonthlyVestingAccount); !ok {
+			continue
+		}
+		fromStr := from.String()
+		value := txData.GetValue()
+		if accountSpendable[fromStr].Cmp(value) < 0 {
+			return ctx, errorsmod.Wrapf(vestingmoduletypes.ErrInsufficientUnlockedCoins, "%s < %s", accountSpendable[fromStr].String(), value.String())
+		}
+		accountSpendable[fromStr] = new(big.Int).Sub(accountSpendable[fromStr], value)
 	}
 	return next(ctx, tx, simulate)
-}
-
-// CheckVesting checks if the account is monthly vesting account and if so,
-// checks that the account has sufficient unlocked balances to cover transaction.
-func CheckVesting(
-	ctx sdk.Context,
-	bankKeeper evmtypes.BankKeeper,
-	account authtypes.AccountI,
-	accountExpenses map[string]*EthVestingExpenseTracker,
-	addedExpense *big.Int,
-	denom string,
-) error {
-	vestingAccount, ok := account.(*vestingmoduletypes.MonthlyVestingAccount)
-	if !ok {
-		return nil
-	}
-
-	// Check to make sure that the account does not exceed its spendable balances.
-	// This transaction would fail in processing, so we should prevent it from
-	// move part the AnteHandler.
-	expenses, err := UpdateAccountExpense(ctx, bankKeeper, accountExpenses, vestingAccount, addedExpense, denom)
-	if err != nil {
-		return err
-	}
-
-	total := expenses.Total
-	spendable := expenses.Spendable
-
-	if total.Cmp(spendable) > 0 {
-		return errorsmod.Wrapf(vestingmoduletypes.ErrInsufficientUnlockedCoins, "%s < %s", spendable.String(), total.String())
-	}
-	return nil
-}
-
-// UpdateAccountExpense updates or sets the total spend for the given account, then
-// returns the new expense.
-func UpdateAccountExpense(
-	ctx sdk.Context,
-	bankKeeper evmtypes.BankKeeper,
-	accountExpenses map[string]*EthVestingExpenseTracker,
-	account *vestingmoduletypes.MonthlyVestingAccount,
-	addedExpense *big.Int,
-	denom string,
-) (*EthVestingExpenseTracker, error) {
-	address := account.GetAddress()
-	addrStr := address.String()
-
-	expenses, ok := accountExpenses[addrStr]
-	// if expense tracker exists, update it by adding new expense
-	if ok {
-		expenses.Total = new(big.Int).Add(expenses.Total, addedExpense)
-		return expenses, nil
-	}
-
-	balance := bankKeeper.GetBalance(ctx, address, denom)
-	if balance.IsZero() {
-		return nil, errorsmod.Wrapf(errortypes.ErrInsufficientFunds,
-			"account has no balance to execute transaction: %s", addrStr)
-	}
-
-	lockedBalances := account.LockedCoins(ctx.BlockTime())
-	ok, lockedBalance := lockedBalances.Find(denom)
-	if !ok {
-		lockedBalance = sdk.NewCoin(denom, math.ZeroInt())
-	}
-
-	spendableValue := big.NewInt(0)
-	if spendableBalance, err := balance.SafeSub(lockedBalance); err == nil {
-		spendableValue = spendableBalance.Amount.BigInt()
-	}
-
-	expenses = &EthVestingExpenseTracker{
-		Total:     addedExpense,
-		Spendable: spendableValue,
-	}
-	accountExpenses[addrStr] = expenses
-
-	return expenses, nil
 }
