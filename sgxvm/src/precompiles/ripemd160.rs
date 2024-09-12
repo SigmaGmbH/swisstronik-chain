d9b94edeb480e492d15475391da38b85f3a62f0c
MikkySnow <mike@swisstronik.com>
2024-09-12 20:10:32 +0200

    refactor: fix compilation issues in precompiles

--- sgxvm/src/precompiles/ripemd160.rs
+++ sgxvm/src/precompiles/ripemd160.rs
@@ -1,10 +1,7 @@
+use evm::interpreter::error::{ExitResult, ExitSucceed};
 use std::vec::Vec;
 use k256::sha2::Digest;
-use crate::precompiles::{
-    ExitSucceed, 
-    LinearCostPrecompile, 
-    PrecompileFailure
-};
+use crate::precompiles::LinearCostPrecompile;
 
 /// The ripemd precompile.
 pub struct Ripemd160;
@@ -13,9 +10,9 @@ impl LinearCostPrecompile for Ripemd160 {
     const BASE: u64 = 600;
     const WORD: u64 = 120;
 
-    fn raw_execute(input: &[u8], _cost: u64) -> Result<(ExitSucceed, Vec<u8>), PrecompileFailure> {
+    fn raw_execute(input: &[u8], _cost: u64) -> (ExitResult, Vec<u8>) {
         let mut ret = [0u8; 32];
         ret[12..32].copy_from_slice(&ripemd::Ripemd160::digest(input));
-        Ok((ExitSucceed::Returned, ret.to_vec()))
+        (ExitSucceed::Returned.into(), ret.to_vec())
     }
 }
--- sgxvm/src/precompiles/sha256.rs
+++ sgxvm/src/precompiles/sha256.rs
@@ -1,13 +1,10 @@
+use evm::interpreter::error::{ExitException, ExitSucceed};
 use std::vec::Vec;
 use k256::sha2::{
     Sha256 as kSha256, 
     Digest
 };
-use crate::precompiles::{
-    ExitSucceed, 
-    LinearCostPrecompile, 
-    PrecompileFailure
-};
+use crate::precompiles::LinearCostPrecompile;
 
 /// The sha256 precompile.
 pub struct Sha256;
@@ -16,10 +13,10 @@ impl LinearCostPrecompile for Sha256 {
     const BASE: u64 = 60;
     const WORD: u64 = 12;
 
-    fn raw_execute(input: &[u8], _cost: u64) -> Result<(ExitSucceed, Vec<u8>), PrecompileFailure> {
+    fn raw_execute(input: &[u8], _cost: u64) -> (ExitException, Vec<u8>) {
         let mut hasher = kSha256::new();
         hasher.update(input);
         let result = hasher.finalize();
-        Ok((ExitSucceed::Returned, result.to_vec()))
+        (ExitSucceed::Returned.into(), result.to_vec())
     }
 }
