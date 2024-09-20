294d945308cb89127249309dbd6bd7ab3ebba5e2
MikkySnow <mike@swisstronik.com>
2024-09-20 12:16:51 +0200

    refactor: bump solidity compiler version and openzeppelin

--- tests/solidity/hardhat.config.js
+++ tests/solidity/hardhat.config.js
@@ -2,7 +2,7 @@ require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-    solidity: "0.8.17",
+    solidity: "0.8.24",
     networks: {
         tronik: {
             url: "http://localhost:8545",
