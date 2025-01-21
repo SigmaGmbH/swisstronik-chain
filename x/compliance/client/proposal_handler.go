101530c8104290c332bc50beed6ec73371a31989
MikkySnow <mike@swisstronik.com>
2025-01-21 15:15:56 +0100

    refactor: disable x/compliance proposal handler

--- x/compliance/client/proposal_handler.go
+++ x/compliance/client/proposal_handler.go
@@ -1,11 +1,5 @@
 package client
 
-import (
-	govclient "github.com/cosmos/cosmos-sdk/x/gov/client"
-
-	"swisstronik/x/compliance/client/cli"
-)
-
 var (
-	VerifyIssuerProposalHandler = govclient.NewProposalHandler(cli.CmdVerifyIssuerProposal)
+// VerifyIssuerProposalHandler = govclient.NewProposalHandler(cli.CmdVerifyIssuerProposal)
 )
