3a53f8bcee8ea94a6c3138e2897efc6456ab1bbb
Software Engineer <mike@swisstronik.com>
2025-02-18 10:15:18 +0100

    feat: Check mainnet hardware (#144)
    * add MAINNET_MODE and CHECKER_MODE env args
    
    * feat: add checker_mode build feature for wrapper
    
    * feat: add build_checker make command
    
    * fix: fix issue with gas calculaton for bn128 pairing

---
+++ go-sgxvm/internal/api/link_glibclinux_x86_64.checker.go
@@ -0,0 +1,6 @@
+//go:build linux && !muslc && amd64 && !sys_sgx_wrapper && !nosgx && checker
+
+package api
+
+// #cgo LDFLAGS: -Wl,-rpath,${SRCDIR} -L${SRCDIR} -lsgx_checker_wrapper_v1.0.7.x86_64
+import "C"
