8d7aca8edd208fb0337239d139c4ffa5a0425ded
MikkySnow <mike@swisstronik.com>
2025-04-10 10:31:36 +0000

    update script to new format csv

--- scripts/genesis/add_regular_accounts.sh
+++ scripts/genesis/add_regular_accounts.sh
@@ -13,6 +13,8 @@ CSV_FILE="$(dirname "$0")/misc/balances.csv"
 GENESIS=$HOMEDIR/config/genesis.json
 TMP_GENESIS=$HOMEDIR/config/tmp_genesis.json
 
+DENOM="swtr"
+
 if [[ -z "$SWTR_BINARY" ]]; then
   BINARY="./build/swisstronikd"
 else
@@ -22,10 +24,10 @@ fi
 # CSV File format looks like as following:
 # -----------
 # address,balance
-# swtr1qa2h6a27waactkrc6dyxrn2jzfjjfg24dgxzu8,30000000000000000000000aswtr
-# swtr1qg9e0d8y9w0z5h7v4x5lq2k3m8n0p6s9y3k5t2,30000000000000000000000aswtr
-# swtr1q8n0p6s9y3k5t2a1b2c3d4e5f6g7h8i9j0k1l2,60000000000000000000000aswtr
-# swtr1q7k5t2a1b2c3d4e5f6g7h8i9j0k1l2m3n4o5p6,120000000000000000000000aswtr
+# swtr1qa2h6a27waactkrc6dyxrn2jzfjjfg24dgxzu8,3000
+# swtr1qg9e0d8y9w0z5h7v4x5lq2k3m8n0p6s9y3k5t2,3000
+# swtr1q8n0p6s9y3k5t2a1b2c3d4e5f6g7h8i9j0k1l2,6000
+# swtr1q7k5t2a1b2c3d4e5f6g7h8i9j0k1l2m3n4o5p6,120000
 # -----------
 
 header_added="\033[32m[ADD]\033[0m"
@@ -38,6 +40,6 @@ while IFS=, read -r address balance; do
         continue
     fi
 
-    $BINARY add-genesis-account "$address" "$balance" --home "$HOMEDIR" --keyring-backend "$KEYRING"
+    $BINARY add-genesis-account "$address" "${balance}${DENOM}" --home "$HOMEDIR" --keyring-backend "$KEYRING"
 
 done < "$CSV_FILE"
