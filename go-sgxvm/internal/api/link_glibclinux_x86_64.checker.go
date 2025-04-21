448b492c086fce6a83ba259c4af408bee13b0cbb
MikkySnow <mike@swisstronik.com>
2025-04-21 11:47:33 +0000

    feat: bump version to v1.0.1

--- go-sgxvm/internal/api/link_glibclinux_x86_64.checker.go
+++ go-sgxvm/internal/api/link_glibclinux_x86_64.checker.go
@@ -2,5 +2,5 @@
 
 package api
 
-// #cgo LDFLAGS: -Wl,-rpath,${SRCDIR} -L${SRCDIR} -lsgx_checker_wrapper_v1.0.0.x86_64
+// #cgo LDFLAGS: -Wl,-rpath,${SRCDIR} -L${SRCDIR} -lsgx_checker_wrapper_v1.0.1.x86_64
 import "C"
