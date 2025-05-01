8e2b82653d5721bdcd91faf894c1f16ac2cefe8d
MikkySnow <mike@swisstronik.com>
2025-05-01 13:40:37 +0000

    refactor: remove EPID-related unused OCALLs

--- sgxvm/src/ocall.rs
+++ sgxvm/src/ocall.rs
@@ -16,25 +16,6 @@ extern "C" {
         _data: *const u8,
         _len: usize) -> sgx_status_t;
 
-    pub fn ocall_get_ias_socket(
-        _ret_val: *mut sgx_status_t,
-        _ret_fd: *mut i32,
-    ) -> sgx_status_t;
-
-    pub fn ocall_get_quote(
-        _ret_val: *mut sgx_status_t,
-        _p_sigrl: *const u8,
-        _sigrl_len: u32,
-        _p_report: *const sgx_report_t,
-        _quote_type: sgx_quote_sign_type_t,
-        _p_spid: *const sgx_spid_t,
-        _p_nonce: *const sgx_quote_nonce_t,
-        _p_qe_report: *mut sgx_report_t,
-        _p_quote: *mut u8,
-        _maxlen: u32,
-        _p_quote_len: *mut u32,
-    ) -> sgx_status_t;
-
     pub fn ocall_get_ecdsa_quote(
 		_ret_val: *mut sgx_status_t,
 		_p_report: *const sgx_report_t,
